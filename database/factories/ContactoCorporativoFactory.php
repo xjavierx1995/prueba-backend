9761bf5b70daf3735330c70b86ef56aac5f0650a
Javier Rodriguez <javier.rodriguez@geor.pe>
2021-02-12 21:08:53 -0400

    tabla contactos_corporativos

---
+++ database/factories/ContactoCorporativoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\ContactoCorporativo;
+use Faker\Generator as Faker;
+
+$factory->define(ContactoCorporativo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
